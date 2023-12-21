54bc5833e1431ba9711efd901bebdcf727096624
Matthew Mason <mmason@medusamedical.com>
2023-12-21 16:26:12 -0400

    initial commit

---
+++ BaseLibrary/BaseLibrary/Exponential.cs
@@ -0,0 +1,15 @@
+﻿namespace BaseLibrary
+{
+    public class Exponential : IGenerator
+    {
+        IEnumerable<int> IGenerator.Generate(int maxIterations)
+        {
+            int ret = 1;
+            for (int i = 0; i < maxIterations; i++)
+            {
+                yield return ret;
+                ret *= 2;
+            }
+        }
+    }
+}
---
+++ BaseLibrary/BaseLibrary/IGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLibrary
+{
+    public interface IGenerator
+    {
+        public IEnumerable<int> Generate(int maxIterations = 10);
+    }
+}
